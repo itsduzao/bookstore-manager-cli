86903439b44d250c6da2393c2bf6bfcfd116ba2c
Eduardo Henrique <itsduzao@gmail.com>
2026-07-10 23:31:35 +0000

    feat: add db schema

---
+++ src/database/schema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS autores (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS clientes (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(150) NOT NULL,
+    email VARCHAR(150) UNIQUE NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS livros (
+    id SERIAL PRIMARY KEY,
+    titulo VARCHAR(200) NOT NULL,
+    quantidade_disponivel INT NOT NULL,
+    autor_id INT NOT NULL,
+    CONSTRAINT fk_autor 
+        FOREIGN KEY (autor_id) 
+        REFERENCES autores(id) 
+        ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS emprestimos (
+    id SERIAL PRIMARY KEY,
+    data_emprestimo DATE NOT NULL DEFAULT CURRENT_DATE,
+    data_vencimento DATE NOT NULL,
+    data_devolucao DATE,
+    cliente_id INT NOT NULL,
+    CONSTRAINT fk_cliente
+        FOREIGN KEY (cliente_id)
+        REFERENCES clientes(id),
+    livro_id INT NOT NULL,
+    CONSTRAINT fk_livro
+        FOREIGN KEY (livro_id)
+        REFERENCES livros(id)
+);
